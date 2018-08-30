bcc01168b9044fda65f63d3010bfc19c348867cd
Brandon Baker <brandonbaker40@icloud.com>
2018-08-30 11:41:55 -0700

    troublshooting pg connection

--- db/create_tables.rb
+++ db/create_tables.rb
@@ -1,24 +1,52 @@
 require 'sqlite3'
+require 'pg'
 
- db = SQLite3::Database.new "db/address_bloc.sqlite"
+#db = SQLite3::Database.new "db/address_bloc.sqlite"
+db = Postgres::Database.new "db/address_bloc.db"
 
- db.execute("DROP TABLE IF EXISTS address_book;");
- db.execute("DROP TABLE IF EXISTS entry;");
 
- db.execute <<-SQL
-     CREATE TABLE address_book (
-       id INTEGER PRIMARY KEY,
-       name VARCHAR(30)
-     );
-   SQL
+####### SQLite3 ########
 
- db.execute <<-SQL
-     CREATE TABLE entry (
-       id INTEGER PRIMARY KEY,
-       address_book_id INTEGER,
-       name VARCHAR(30),
-       phone_number VARCHAR(30),
-       email VARCHAR(30),
-       FOREIGN KEY (address_book_id) REFERENCES address_book(id)
-     );
-   SQL
+db.execute("DROP TABLE IF EXISTS address_book;");
+db.execute("DROP TABLE IF EXISTS entry;");
+
+db.execute <<-SQL
+    CREATE TABLE address_book (
+      id INTEGER PRIMARY KEY,
+      name VARCHAR(30),
+    );
+  SQL
+
+db.execute <<-SQL
+    CREATE TABLE entry (
+      id INTEGER PRIMARY KEY,
+      address_book_id INTEGER,
+      name VARCHAR(30),
+      phone_number VARCHAR(30),
+      email VARCHAR(30),
+      FOREIGN KEY (address_book_id) REFERENCES address_book(id)
+    );
+  SQL
+
+  ######## Postgres #########
+
+  db.exec("DROP TABLE IF EXISTS address_book;");
+  db.exec("DROP TABLE IF EXISTS entry;");
+
+  db.exec <<-SQL
+    CREATE TABLE address_book (
+      id INT PRIMARY KEY NOT NULL,
+      name TEXT NOT NULL
+    );
+  SQL
+
+  db.exec <<-SQL
+    CREATE TABLE entry (
+      id INT PRIMARY KEY NOT NULL,
+      address_book_id INT NOT NULL,
+      name CHAR(30),
+      phone_number CHAR(30),
+      email CHAR(30),
+      FOREIGN KEY (address_book_id) REFERENCES address_book(id)
+    );
+  SQL
--- db/seed.rb
+++ db/seed.rb
@@ -2,7 +2,9 @@ require_relative '../models/address_book'
  require_relative '../models/entry'
  require 'bloc_record'
 
- BlocRecord.connect_to('db/address_bloc.sqlite')
+ #BlocRecord.connect_to('db/address_bloc.sqlite', :sqlite3)
+ BlocRecord.connect_to('db/address_bloc.db', :pg)
+
 
  book = AddressBook.create(name: 'My Address Book')
 
